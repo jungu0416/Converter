0a5b5f7a12c9f638ff466bd58b1b51785c11852b
jungu0416 <jungu0416@naver.com>
2022-11-01 17:38:27 +0900

    convert ...

--- src/main/java/co/kr/convert/data/Migration.java
+++ src/main/java/co/kr/convert/data/Migration.java
@@ -1,8 +1,8 @@
 package co.kr.convert.data;
 
 public class Migration {
-    public static String[] NAMES = {"ecod20s"}; //테스트용
-    //public static String[] NAMES = {"ecod20","ecod20s","ecod21","ecod40","ecod40s"};
+    //public static String[] NAMES = {"ecod20s"}; //테스트용
+    public static String[] NAMES = {"ecod20","ecod20s","ecod21","ecod40","ecod40s"};
     public static String defaultDir = "\\\\192.168.10.12\\share\\EMS-DB\\data\\paradox\\";
 
 }
